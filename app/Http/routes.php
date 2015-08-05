b1b87157d87d58e8a795a35c7c8ea5342907c6af
uyab <uyab.exe@gmail.com>
2015-08-05 15:12:45 +0700

    add home

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('home', function () {
+    return 'Welcome home, ' . auth()->user()->name;
+});
+
 // Authentication routes...
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
