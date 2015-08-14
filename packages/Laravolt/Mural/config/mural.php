8d1de6b836c6d853e49ad46e9b0e95be413a8da1
uyab <uyab.exe@gmail.com>
2015-08-14 14:42:41 +0700

    remove local package for mural

--- packages/Laravolt/Mural/config/mural.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'skin'                => 'semantic-ui',
-    'default_commentable' => \App\Models\Post::class,
-    'per_page'            => 5
-];
--- packages/Laravolt/Mural/resources/views/semantic-ui/list.blade.php
+++
@@ -1,3 +0,0 @@
-@foreach($comments as $comment)
-    @include('mural::item')
-@endforeach
