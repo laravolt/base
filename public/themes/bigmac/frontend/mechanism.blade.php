3ea259f1d7cd69e325c1e75afd362868cc3f6b79
uyab <uyab.exe@gmail.com>
2015-08-25 19:08:32 +0700

    styling mechanism

--- public/themes/bigmac/frontend/mechanism.blade.php
+++ public/themes/bigmac/frontend/mechanism.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.frontend')
 
 @section('content')
-    <div class="title">
-        <h2 class="ui header">MEKANISME</h2>
-    </div>
-    <div class="ui container">
+    <div id="page-mechanism">
+        <div class="title">
+            <h2 class="ui header">MEKANISME</h2>
+        </div>
+        <div class="ui container">
+            <div class="ui segment outer">
+                <div class="ui segment inner">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, quasi, temporibus. Dolorem ea incidunt maxime quidem, repellat repudiandae sed sequi unde? At delectus fuga harum labore quos sapiente ut voluptatem.
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
