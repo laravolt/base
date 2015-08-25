1fd1be9f34d6c844ce461c591c74f64e00b45280
uyab <uyab.exe@gmail.com>
2015-08-25 18:58:15 +0700

    styling gallery page

--- public/themes/bigmac/frontend/gallery.blade.php
+++ public/themes/bigmac/frontend/gallery.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.frontend')
 
 @section('content')
-    <div class="title">
-        <h2 class="ui header">Galeri <strong>#BIGMACforaBIGDAY</strong></h2>
-    </div>
-    <div class="ui container">
+    <div id="page-gallery">
+        <div class="title">
+            <h2 class="ui header">Galeri <strong>#BIGMACforaBIGDAY</strong></h2>
+        </div>
+        <div class="ui container">
+            <div class="ui equal width padded grid">
+                @foreach(range(1, 4) as $item)
+                    @foreach(range(1, 4) as $item)
+                        <div class="eight wide mobile four wide tablet four wide computer column">
+                            <img src="{{ theme_asset('img/meme.png') }}" class="ui image fluid">
+                        </div>
+                    @endforeach
+                @endforeach
+            </div>
+        </div>
     </div>
 @endsection
